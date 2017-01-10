f314e0d4f289d10f0e4090f9577df02c1fbe278f
Jeremy Foster <jeremy.foster@live.com>
2017-01-10 10:31:35 -0800

    combine service and command-central into one service

--- service/index.js
+++ service/index.js
@@ -1,13 +1,17 @@
+var iothub = require('azure-iothub');
+var common = require('azure-iot-common');
 let eventhubs = require('azure-event-hubs');
-var client = eventhubs.Client.fromConnectionString('HostName=airbus-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=sELHu1v/5q1Br7VR0j14C9oV/yeXT3Ucb3nxxjN1v1o=');
 
-client.open()
+var eventhubsClient = eventhubs.Client.fromConnectionString('HostName=airbus-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=sELHu1v/5q1Br7VR0j14C9oV/yeXT3Ucb3nxxjN1v1o=');
+var iothubClient = iothub.Client.fromConnectionString('HostName=airbus-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=sELHu1v/5q1Br7VR0j14C9oV/yeXT3Ucb3nxxjN1v1o=');
+
+eventhubsClient.open()
     .then(() => console.log('ready'))
-    .then(client.getPartitionIds.bind(client))
+    .then(eventhubsClient.getPartitionIds.bind(eventhubsClient))
     .then(pids =>
         //enumerate the partitions
         pids.map(pid =>
-            client.createReceiver('$Default', pid, { 'startAfterTime': Date.now() })
+            eventhubsClient.createReceiver('$Default', pid, { 'startAfterTime': Date.now() })
                 .then(receiver =>
                     //handle D2C messages
                     receiver.on('message', msg =>
@@ -16,3 +20,12 @@ client.open()
                 )
         )
     );
+
+iothubClient.open(err => {
+    // send a C2D message repeatedly
+    setInterval(() => {
+      var message = new common.Message(JSON.stringify({ text: 'reset-request' }));
+      console.log('Sending C2D message -->');
+      iothubClient.send('device1', message, (err, res) => { });
+    }, 5000);
+});
