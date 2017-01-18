91a2b370acbbd004363c804999c36c92396b29fe
Jeremy Foster <jeremy.foster@live.com>
2017-01-18 09:48:05 -0800

    retarget iot hub

--- device/index.js
+++ device/index.js
@@ -1,6 +1,6 @@
 let deviceAmqp = require('azure-iot-device-amqp');
 var device = require('azure-iot-device');
-var client = deviceAmqp.clientFromConnectionString('HostName=airbus-hub.azure-devices.net;DeviceId=device1;SharedAccessKey=YVFhWIXNnf62BMsr1BsfRfFtA2JtbcE00vZwMk8+53M=');
+var client = deviceAmqp.clientFromConnectionString('HostName=dmg-demo.azure-devices.net;DeviceId=device1;SharedAccessKey=31mHmA3FckeAFa821HBneGeiuMc4UHHabST9FnyYh+A=');
 
 client.open(err => {
     //handle C2D messages
